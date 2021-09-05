b871de8325262a9b44a68a7a37d4e437dbbe7921
Abdullah Alhazmi <alhazmiabdullah@outlook.com>
2021-09-05 10:23:52 +0300

    handle sending password and authenticating user

--- server/app.js
+++ server/app.js
@@ -4,14 +4,13 @@ const app = express();
 const port = process.env.PORT || 4001;
 const server = app.listen(port, () => console.log(`Listening on port ${port}`));
 
-
 const bodyParser = require("body-parser");
 const socketio = require("socket.io")(server, {
   cors: {
     origin: "http://localhost:3000",
     methods: ["GET", "POST"],
-    credentials: true
-  }
+    credentials: true,
+  },
 });
 
 // const io = socketio(server);
@@ -20,45 +19,51 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-
 //const io = socketIo(server); // < Interesting!
 
 app.get("/", (req, res) => {
   res.send("GG");
-
 });
 
-
-
 socketio.on("connection", (socket) => {
-    console.log("New client connected");
+  console.log("New client connected");
 
-   socket.on("Message", (mes) => {
-       console.log("message: \n" + mes);
-       payload = JSON.parse(mes);
-       socket.broadcast.emit("new message", payload);
-   });
-   
-   socket.on("new password", (payload, ack) => {
-       console.log("message: \n" + payload);
-       ack("");
-       socket.broadcast.emit("Password", payload);
-   });
-   
-   socket.on("new mode", (payload, ack) => {
-       console.log("message: \n" + payload);
-       ack(payload);
-       socket.broadcast.emit("Mode", payload);
-   });
-   
-   socket.on("Authentication", (payload) => {
-       console.log("message: \n" + payload);
-       socket.broadcast.emit("authentication", payload);
-   });
-   
-    socket.on("disconnect", () => {
-      console.log("Client disconnected");
-      
-    });
+  socket.on("Message", (mes) => {
+    console.log("message: \n" + mes);
+    payload = JSON.parse(mes);
+    socket.broadcast.emit("new message", payload);
   });
-  
+
+  socket.on("new password", (payload, ack) => {
+    console.log("message: \n" + payload);
+    ack("");
+    socket.broadcast.emit("Password", payload);
+    socket.broadcast.emit("Authenticated", true);
+  });
+
+  socket.on("new mode", (payload, ack) => {
+    console.log("message: \n" + payload);
+    ack(payload);
+    socket.broadcast.emit("Mode", payload);
+  });
+
+  socket.on("Authentication", (payload) => {
+    console.log("message: \n" + payload);
+    socket.broadcast.emit("authentication", payload);
+  });
+  socket.on("Authenticated", (payload) => {
+    console.log("message: \n" + payload);
+    //TODO: change the emitted value
+    socket.broadcast.emit("Authenticated", payload);
+  });
+
+  socket.on("Logged Out", (payload) => {
+    console.log("message: \n" + payload);
+    //TODO: change the emitted value
+    socket.broadcast.emit("Logged Out", false);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+});
